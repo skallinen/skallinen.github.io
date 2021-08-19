2ab17a4cae2afbe36f0c6f8c0181334ca186d923
Sami Kallinen <notjustsilicon@gmail.com>
2021-08-19 18:38:30 +0300

    testing

--- various/init.el
+++ various/init.el
@@ -1,20 +1,23 @@
-(setq custom-file "~/.custom.el")
+(load-theme 'tango-dark nil)
 
-;; Install straight.el
-;; Just pasted from https://github.com/raxod502/straight.el#getting-started
-(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
-      (bootstrap-version 3))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
 
-;; You have to install use-package in order to use use-package with straight
-(straight-use-package 'use-package)
+;; (setq custom-file "~/.custom.el")
 
-(use-package cider
- :straight t)
+;; ;; Install straight.el
+;; ;; Just pasted from https://github.com/raxod502/straight.el#getting-started
+;; (let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+;;       (bootstrap-version 3))
+;;   (unless (file-exists-p bootstrap-file)
+;;     (with-current-buffer
+;;         (url-retrieve-synchronously
+;;          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+;;          'silent 'inhibit-cookies)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp)))
+;;   (load bootstrap-file nil 'nomessage))
+
+;; ;; You have to install use-package in order to use use-package with straight
+;; (straight-use-package 'use-package)
+
+;; (use-package cider
+;;  :straight t)
