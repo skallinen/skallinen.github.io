6f9366f76c8cde02492ca14e999a4c60ba7e043b
Sami Kallinen <notjustsilicon@gmail.com>
2021-08-19 17:09:07 +0300

    test emacs config

--- various/init.el
+++ various/init.el
@@ -1,1 +1,20 @@
-(load-theme 'tango-dark nil)
+(setq custom-file "~/.custom.el")
+
+;; Install straight.el
+;; Just pasted from https://github.com/raxod502/straight.el#getting-started
+(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+      (bootstrap-version 3))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; You have to install use-package in order to use use-package with straight
+(straight-use-package 'use-package)
+
+(use-package cider
+ :straight t)
