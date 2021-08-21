1fc4df6b4557ace61b09334b16a89ea4e4c4838f
Sami Kallinen <notjustsilicon@gmail.com>
2021-08-21 08:47:59 +0300

    deps.edn

--- various/init.el
+++ various/init.el
@@ -1,12 +1,11 @@
 (progn
   (setq inhibit-startup-message t)
-
   (scroll-bar-mode -1)        ; Disable visible scrollbar
   (tool-bar-mode -1)          ; Disable the toolbar
   (tooltip-mode -1)           ; Disable tooltips
   (set-fringe-mode 10)        ; Give some breathing room
-
   (menu-bar-mode -1)            ; Disable the menu bar
+
   (setq custom-file "~/.custom.el")
 
   ;; Install straight.el
