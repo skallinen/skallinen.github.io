638f236d5d75017e10cd2d05b4faeca6e7829c5f
Sami Kallinen <notjustsilicon@gmail.com>
2021-10-18 11:54:29 +0300

    fix api key

--- various/map-demo/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { map_demo.core.init(); } catch (e) { console.error("An error occurred when calling (map-demo.core/init)"); throw(e); }
